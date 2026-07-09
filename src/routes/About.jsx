dea91b0be39548f25db012da7ade637155f6bc4a
Andre <andrebayucan@gmail.com>
2026-07-09 16:14:15 -0700

    Updated About section

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -2,7 +2,11 @@ import './About.css'
 
 const About = () => {
     return (
-        <div classname="about-box">
+        <div className="about-box">
+            <h1 className="bold-text">🌼 What is this site? 🌼</h1>
+            <h2>Welcome to <span className="bold-text">Life Dashboard!</span> Discover a trove of info regarding organisms observed around the world.</h2>
+            <p>The main home page provides filter functionalities through an observations upper-limit slider and a search bar. To view more info about an organism, simply click on its name in the statistics list.</p>
+            <p>Placeholder text for charts</p>
         </div>
     )
 }
